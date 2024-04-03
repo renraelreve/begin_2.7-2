623f9c69fc545a1bafb004b3dda0229d43df5115
Ng Teck Guan <renraelreve@gmail.com>
2024-04-04 00:47:39 +0800

    finally manage to update the original list

---
+++ src/components/EditForm.js
@@ -0,0 +1,80 @@
+import styles from "./ViewList.module.css";
+
+import { useContext } from "react";
+import ModeContext from "../context/ModeContext";
+
+function EditForm({ form, editForm, updateList }) {
+  const modeCtx = useContext(ModeContext);
+
+  const updateName = (name) => {
+    editForm((form) => ({ ...form, name: name }));
+  };
+
+  const updateQuantity = (quantity) => {
+    editForm((form) => ({ ...form, quantity: quantity }));
+  };
+
+  const updatePrice = (price) => {
+    editForm((form) => ({ ...form, price: price }));
+  };
+
+  const updateDiscount = (discount) => {
+    editForm((form) => ({ ...form, discount: discount }));
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    updateList(form.id);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <table className={`${styles.table} ${!modeCtx.isLight && styles.dark}`}>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Qty</th>
+              <th>Price</th>
+              <th>Disc %</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  value={form.name}
+                  onChange={(e) => updateName(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={form.quantity}
+                  onChange={(e) => updateQuantity(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={form.price}
+                  onChange={(e) => updatePrice(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={form.discount}
+                  onChange={(e) => updateDiscount(e.target.value)}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button>Update</button>
+    </form>
+  );
+}
+export default EditForm;
---
+++ src/components/Product.js
@@ -0,0 +1,96 @@
+import { v4 as uuid } from "uuid";
+import { useState, useContext, useMemo } from "react";
+
+import styles from "./Product.module.css";
+import Card from "./Card";
+import ViewList from "./ViewList";
+import EditForm from "./EditForm";
+
+import ProductContext from "../context/ProductContext";
+import ModeContext from "../context/ModeContext";
+import Toggle from "./Toggle";
+
+function Product() {
+  const ctx = useContext(ProductContext);
+  const modeCtx = useContext(ModeContext);
+  const [list, setList] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    quantity: 0,
+    price: 0,
+    discount: 0,
+  });
+  // const [sumTotal, setSumTotal] = useState(0);
+
+  /*
+    CREATE: Add a new product into the list
+  */
+  const handlerAddProduct = () => {
+    const newItem = {
+      id: uuid(), // Call uuid() here
+      name: ctx.name,
+      quantity: ctx.count,
+      price: ctx.price,
+      discount: ctx.discount,
+      total: (ctx.count * ctx.price * (100 - ctx.discount)) / 100,
+    };
+    const newList = [...list, newItem];
+    setList(newList);
+    // const sum = sumTotal + newItem.total;
+    // setSumTotal(sum);
+  };
+
+  const handlerEditProduct = (id) => {
+    const newForm = list.filter((item) => item.id === id);
+    setForm(newForm[0]);
+  };
+
+  const handlerDeleteProduct = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+    // const sum = newList.reduce((acc, i) => acc + i.total, 0);
+    // setSumTotal(sum);
+  };
+
+  const total = useMemo(() => {
+    return list.reduce(
+      (accumulated, currentItem) => accumulated + currentItem.total,
+      0
+    );
+  }, [list]);
+
+  const updateList = (id) => {
+    const newList = list.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: form.quantity,
+          name: form.name,
+          price: form.price,
+          discount: form.discount,
+        };
+      }
+      return item; // Return the original item if the condition doesn't match
+    });
+    // Now you should do something with newList, for example, update the state
+    setList(newList);
+  };
+
+  //---------------------------------------------------------------------------
+
+  return (
+    <div className={`${styles.container} ${modeCtx.isDark && styles.dark}`}>
+      <Toggle />
+      <Card handlerAddProduct={handlerAddProduct} />
+      <ViewList
+        list={list}
+        sum={total}
+        handlerDeleteItem={handlerDeleteProduct}
+        handlerEditItem={handlerEditProduct}
+      />
+      <EditForm form={form} editForm={setForm} updateList={updateList} />
+    </div>
+  );
+}
+
+export default Product;
